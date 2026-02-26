0fa49b12395b34c740f63c4ef1f20b7fe67d64fc
mzazitech <mzazitech001@gmail.com>
2026-02-27 02:18:43 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
-import "./globals.css"
+import "./globals.css";
 
 export const metadata = {
-  title: "Mzazigo Kenya",
-  description: "Ride Booking Platform - Taxi & Boda"
-}
+  title: "Mzazigo",
+  description: "Ride anywhere with Mzazigo",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
